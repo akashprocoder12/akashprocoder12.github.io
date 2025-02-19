67970d2a91d74c7c5dd3c8eba01fd8be76506c9f
akash pandey <akashpandey23aids@rnsit.ac.in>
2025-02-19 12:10:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const speed_factor = 5;
 function drawBackground() {
     
 
-    ctx.strokeStyle = "rgba(16, 0, 140, 0.78)";
+    ctx.strokeStyle = "rgb(16, 0, 140)";
     ctx.lineWidth = 1;
     for (let i = 0; i <= canvas.width; i += 50) {
         ctx.beginPath();
@@ -24,6 +24,8 @@ function drawBackground() {
 
 
 
+
+
 class Write_A {
   constructor() {
     this.ball_one = { x: 0.1 * canvas.width, y: (20 / 300) * canvas.height };
@@ -38,7 +40,7 @@ class Write_A {
   draw(x, y) {
     ctx.beginPath();
     ctx.arc(x, y, 10, 0, Math.PI * 2);
-    ctx.fillStyle = "gray";
+    ctx.fillStyle = `rgb(${this.vertical_distance},0,${this.vertical_distance})`;
     ctx.fill();
   }
 
@@ -87,7 +89,7 @@ class write_K {
   draw(x, y) {
     ctx.beginPath();
     ctx.arc(x, y, 10, 0, Math.PI * 2);
-    ctx.fillStyle = "gray";
+    ctx.fillStyle = `rgb(${this.vertical_distance},0,${this.vertical_distance})`;
     ctx.fill();
   }
 
@@ -132,7 +134,7 @@ class Write_A_2 {
   draw(x, y) {
     ctx.beginPath();
     ctx.arc(x, y, 10, 0, Math.PI * 2);
-    ctx.fillStyle = "gray";
+    ctx.fillStyle = `rgb(${this.vertical_distance},0,${this.vertical_distance})`;
     ctx.fill();
   }
 
@@ -178,7 +180,7 @@ class Write_S {
     draw(x, y) {
         ctx.beginPath();
         ctx.arc(x, y, 10, 0, Math.PI * 2);
-        ctx.fillStyle = "gray";
+        ctx.fillStyle = `rgb(${this.vertical_distance},0,${this.vertical_distance})`;
         ctx.fill();
     }
     // 
@@ -240,7 +242,7 @@ class Write_H {
   draw(x, y) {
     ctx.beginPath();
     ctx.arc(x, y, 10, 0, Math.PI * 2);
-    ctx.fillStyle = "gray";
+    ctx.fillStyle = `rgb(${this.vertical_distance},0,${this.vertical_distance})`;
     ctx.fill();
   }
 
@@ -274,102 +276,9 @@ class Write_H {
   }
 }
 
-class Particle {
-    constructor(x, y) {
-        this.x = x;
-        this.y = y;
-        this.velX = (Math.random() - 0.5) * 0.2;
-        this.velY = -(Math.random() * 3 + 2);
-        this.size = Math.random() * 10 + 5;
-        this.life = 1;
-        this.decay = Math.random() * 0.015 + 0.015;
-        this.color = `hsl(${Math.random() * 20 + 30}, 100%, ${Math.random() * 30 + 50}%)`;
-    }
-
-    update() {
-        this.x += this.velX + Math.sin(Date.now() * 0.001) * 0.3;
-        this.y += this.velY;
-        this.velY += 0.1;
-        this.life -= this.decay;
-        this.size *= 0.98;
-    }
-
-    draw() {
-        ctx.globalAlpha = this.life;
-        ctx.beginPath();
-        ctx.fillStyle = this.color;
-        ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
-        ctx.fill();
-    }
-}
 
 
-class Fire {
-    constructor() {
-        this.particles = [];
-        this.maxParticles = 200;
-        this.spawnRate = 5;
-        this.baseX = canvas.width / 2;
-        this.baseY = canvas.height - 50;
-    }
 
-    spawn() {
-        for (let i = 0; i < this.spawnRate; i++) {
-            if (this.particles.length < this.maxParticles) {
-                this.particles.push(new Particle(
-                    this.baseX + (Math.random() - 0.5) * 100,
-                    this.baseY
-                ));
-            }
-        }
-    }
-
-    update() {
-        this.spawn();
-        for (let i = this.particles.length - 1; i >= 0; i--) {
-            this.particles[i].update();
-            if (this.particles[i].life <= 0) {
-                this.particles.splice(i, 1);
-            }
-        }
-    }
-
-    draw() {
-        ctx.globalCompositeOperation = 'lighter';
-        this.particles.forEach(particle => particle.draw());
-        ctx.globalCompositeOperation = 'source-over';
-        
-        // Draw glowing base
-        const gradient = ctx.createRadialGradient(
-            this.baseX, this.baseY, 0,
-            this.baseX, this.baseY, 100
-        );
-        gradient.addColorStop(0, 'rgba(255, 100, 0, 0.8)');
-        gradient.addColorStop(1, 'rgba(0, 0, 0, 0)');
-        ctx.fillStyle = gradient;
-        ctx.fillRect(this.baseX - 100, this.baseY - 100, 200, 200);
-    }
-}
-
-const fire = new Fire();
-
-
-// function animate_two() {
-//     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    
-//     // Draw background gradient
-//     const gradient = ctx.createLinearGradient(0, 0, 0, canvas.height);
-//     gradient.addColorStop(0, 'rgba(0, 0, 0, 0.8)');
-//     gradient.addColorStop(1, 'rgba(50, 20, 0, 0.5)');
-//     ctx.fillStyle = gradient;
-//     ctx.fillRect(0, 0, canvas.width, canvas.height);
-
-//     fire.update();
-//     fire.draw();
-    
-//     requestAnimationFrame(animate_two);
-// }
-// animate_two();
 
 
 function partition_through_line() {
@@ -384,8 +293,6 @@ function partition_through_line() {
   }
 }
 
-partition_through_line();
-drawBackground();
 
 const A = new Write_A();
 const K = new write_K();
@@ -399,7 +306,67 @@ function animate() {
   A2.draw_A_two();
   S.draw_S();
   H.draw_H();
+  if ( A.vertical_distance > 1000){
+    restore();
+  }
+  
 }
 
+
 animate();
+partition_through_line();
+drawBackground();
+
+
+function restore(){
+  ctx.clearRect(0,0,canvas.width,canvas.height)
+  partition_through_line();
+  drawBackground();
+  A.ball_one = { x: 0.1 * canvas.width, y: (20 / 300) * canvas.height };
+  A.ball_two = {
+    x: (46.2 / 1500) * canvas.width,
+    y: (279.5 / 300) * canvas.height,
+  };
+  A.ball_three = { x: 0.05 * canvas.width, y: canvas.height / 1.8 };
+  A.vertical_distance = 0;
+  K.ball_one = {
+    x: canvas.width * (1 / 5 + 1 / 20),
+    y: (20 / 300) * canvas.height,
+  };
+  K.ball_two = {
+    x: canvas.width * (1 / 5 + 3 / 20),
+    y: (20 / 300) * canvas.height,
+  };
+  K.vertical_distance = 0;
+  A2.ball_one = { x: 0.5 * canvas.width, y: 20 };
+    A2.ball_two = {
+      x: (2 * canvas.width) / 5 + 46.2,
+      y: (279.5 / 300) * canvas.height,
+    };
+    A2.ball_three = {
+      x: (2 / 5 + 0.05) * canvas.width,
+      y: canvas.height / 1.8,
+    };
+    A2.vertical_distance = 0;
+    S.ball_one = { x: (3/5 + 3/20) * canvas.width, y: 20/300 * canvas.height };
+        
+        S.vertical_distance = 0;
+        H.ball_one = {
+          x: (4 / 5 + 1 / 20) * canvas.width,
+          y: (20 / 300) * canvas.height,
+        };
+        H.ball_two = {
+          x: (4 / 5 + 3 / 20) * canvas.width,
+          y: (279.5 / 300) * canvas.height,
+        };
+        H.ball_three = {
+          x: (4 / 5 + 0.05) * canvas.width,
+          y: canvas.height / 1.8,
+        };
+        H.vertical_distance = 0;
+
+
+
+}
+
 
